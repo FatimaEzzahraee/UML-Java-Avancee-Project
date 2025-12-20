afeac03cb942eca8f87fb382bd4fa3f6a15edd5f
unknown <bouhassaninasse@gmail.com>
2025-12-20 19:32:02 +0100

    réalisation des packages model et service

--- source/LIB/src/main/java/lib/service/UtilisateurService.java
+++ source/LIB/src/main/java/lib/service/UtilisateurService.java
@@ -1,5 +1,10 @@
-package lib.service;
+package service;
+
+import model.Utilisateur;
 
 public class UtilisateurService {
 
+    public boolean estAdmin(Utilisateur u) {
+        return u.getRole().equalsIgnoreCase("ADMIN");
+    }
 }
